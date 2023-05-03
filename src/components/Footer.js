b9ec01b73c514ba60147a6fa17d9e8df8b9c5f72
Kapeel Mopkar <kapeel.mopkar@vuclip.com>
2023-05-03 13:14:40 +0530

    Revamped todo UI

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+export default function Footer() {
+    return ( <div className="fixed-footer">
+    <div className="container">
+        Copyright &copy; 2023 Kapeel Mopkar</div>        
+    </div>);
+}
